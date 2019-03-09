01513e10b3587654d36ffd667a76610e9a0592ee
Bogumila Romanowska <bogurom@gmail.com>
2019-03-09 15:23:13 +0100

    Remove var argComputerMove

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function buttonClicked(argButtonName) {
   console.log(argButtonName + ' został kliknięty');
 
 
-  var argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+  var computerMove, playerMove, randomNumber, playerInput;
 
   /**
    * Describe this function...
